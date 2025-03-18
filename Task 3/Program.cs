b09cdc74e9e3f9150bc7b43699a3fcbfffffcf94
byaviator <abdullahmustafayev@icloud.com>
2025-03-19 03:05:22 +0400

    Task 3 done

---
+++ Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name :");
+            var name = Console.ReadLine();
+            Console.WriteLine("Surname :");
+            var surname = Console.ReadLine();
+            Console.WriteLine("Username :");
+            var username = Console.ReadLine();
+            Console.WriteLine("Password :");
+            var password = Console.ReadLine();
+            user user = new user(name,surname,username,password);
+            user.age = 20;
+            user.ShowFullData();
+
+
+        }
+    }
+}
---
+++ Task 3/human.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3;
+
+class human
+{
+    public string name;
+    public string surname;
+    private byte _age;
+    public byte age
+{
+    get { return _age; }
+    set { _age = value; }
+}
+
+    public human()
+    {
+       
+    }
+
+    public human(string name)
+    {
+        this.name = name;
+        
+    }
+
+    public human(string name, string surname )
+    {
+        this.name = name;
+        this.surname = surname;
+    }
+
+
+    public void ShowFullData()
+    {
+        Console.WriteLine($" Name : {name}\n Surname : {surname}\n Age : {age}");
+    }
+
+    public string GetFullName()
+    {
+        return $"{name} {surname}";
+    }
+}
+
+
+
+class user :human
+{
+    public string UserName;
+    public string Password;
+
+
+    public user(string name,string surname,string username,string password) : base(name,surname)
+    {
+        if (username.Length <6 || username.Length >25)
+            Console.WriteLine(username);
+        if (password.Length < 8 || password.Length > 25)
+            Console.WriteLine(password);
+
+        UserName = username;
+        Password = password;
+
+    }
+   
+
+}
+ 
+
+
+
+
